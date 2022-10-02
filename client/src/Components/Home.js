f0f4ef2de5b4bb9b879ecd64ec7e86c175445952
Gurjinder Singh Bhandal <g.singh.bhandal@outlook.com>
2022-10-02 00:18:16 -0400

    Fixed blank screen > index.js > from "react-dom"

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,12 +1,12 @@
 import styled, { keyframes } from "styled-components";
 
 const Home = () => {
-  return <Div>This is the homepage</Div>;
+  return <Div>This is the homepage! </Div>;
 };
 
 export default Home;
 
 const Div = styled.div`
-  height: 100vh;
+  height: 800px;
   border: 1px solid red;
 `;
